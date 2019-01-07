7b41672a57bedd962684b52e6aa7b77657a2cd49
moxdev <moxdev1@gmail.com>
2019-01-07 16:57:15 -0500

    redo mobile menu

--- header.php
+++ header.php
@@ -25,29 +25,34 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'quantum' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-				the_custom_logo();
-			?>
-		</div><!-- .site-branding -->
+		<div>
 
-		<?php if ( has_nav_menu( 'main' ) ) : ?>
-			<button class="menu-toggle" aria-controls="main-menu" aria-expanded="false"><span class="screen-reader-text">Menu</span></button>
-		<?php endif; ?>
-
-		<?php if ( has_nav_menu( 'main' ) ) : ?>
-			<nav id="site-navigation" class="main-navigation">
+			<div class="site-branding">
 				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'main',
-						'container'      => '',
-						'menu_id'        => 'main-menu',
-					)
-				);
+					the_custom_logo();
 				?>
-			</nav><!-- #site-navigation -->
-		<?php endif; ?>
+			</div><!-- .site-branding -->
+
+			<?php if ( has_nav_menu( 'main' ) ) : ?>
+				<button class="menu-toggle" aria-controls="main-menu" aria-expanded="false"><span class="screen-reader-text">Menu</span></button>
+			<?php endif; ?>
+
+			<?php if ( has_nav_menu( 'main' ) ) : ?>
+				<nav id="site-navigation" class="main-navigation">
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'main',
+							'container'      => '',
+							'menu_id'        => 'main-menu',
+						)
+					);
+					?>
+				</nav><!-- #site-navigation -->
+			<?php endif; ?>
+
+		</div>
+
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
