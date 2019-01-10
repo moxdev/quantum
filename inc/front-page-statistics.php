f4e75831ca6b306901a0167bff1aaff7f977eaac
moxdev <moxdev1@gmail.com>
2019-01-10 16:28:38 -0500

    stats section mobile done

--- inc/front-page-statistics.php
+++ inc/front-page-statistics.php
@@ -11,7 +11,7 @@ function quantum_flexible_statistics_content() {
 	if ( have_rows( 'statistics_section' ) ) : ?>
 
 		<div class="statistics-content">
-			<div class="statistics-container">
+
 				<?php
 				while ( have_rows( 'statistics_section' ) ) :
 					the_row();
@@ -20,16 +20,17 @@ function quantum_flexible_statistics_content() {
 					$statistic_description = get_sub_field( 'statistic_description' );
 					?>
 
-					<div class='icon-wrapper'>
-						<img src="<?php echo esc_url( $statistic_icon['sizes']['statistics-icon'] ); ?>" alt="<?php echo esc_attr( $statistic_icon['alt'] ); ?>" description="<?php echo esc_attr( $statistic_icon['description'] ); ?>">
-					</div>
-					<div class='description-wrapper'>
-						<span class='number'><?php echo esc_html( $statistic_number ) ?></span>
-						<span class='description'><?php echo esc_html( $statistic_description ) ?></span>
+					<div class="statistics-container">
+						<div class='icon-wrapper'>
+							<img src="<?php echo esc_url( $statistic_icon['sizes']['statistics-icon'] ); ?>" alt="<?php echo esc_attr( $statistic_icon['alt'] ); ?>" description="<?php echo esc_attr( $statistic_icon['description'] ); ?>">
+						</div>
+						<div class='description-wrapper'>
+							<span class='number'><?php echo esc_html( $statistic_number ) ?></span>
+							<span class='description'><?php echo esc_html( $statistic_description ) ?></span>
+						</div>
 					</div>
 
 				<?php endwhile; ?>
-			</div>
 		</div>
 		<?php
 	endif;
